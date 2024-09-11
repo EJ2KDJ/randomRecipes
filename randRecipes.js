591b4ca20c5fcc098cb481a73718bc36aadb87fb
ej2kdj <emmanuelle.salga2007@gmail.com>
2024-09-11 23:56:38 +0800

    function messages for randomizer

---
+++ randRecipes.js
@@ -0,0 +1,27 @@
+function randomRecipe() {
+    const randIngredient = [
+        "pasta",
+        "chicken",
+        "bacon",
+        "beef",
+        "tuna",
+        "potatoes"   
+    ];
+
+    const randCookingMethod = [
+        "fry",
+        "boil",
+        "grill",
+        "roast",
+        "steam"
+        ]
+
+    const randUtensil = [
+        "spatula",
+        "tongs",
+        "pot",
+        "pan",
+        "microwave",
+        "pressure cooker"
+    ]
+}
