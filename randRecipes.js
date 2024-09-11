d29c7b351dba2c8a877cdb0147c0a2b8d5929498
ej2kdj <emmanuelle.salga2007@gmail.com>
2024-09-12 00:18:00 +0800

    program kept running only the frying cooking method because I forgot to push .length in the randomizer variable.

--- randRecipes.js
+++ randRecipes.js
@@ -27,7 +27,7 @@ function randomRecipe() {
 
 const time = Math.floor(Math.random() * 10) + 1;
 const ingredient = randIngredient[Math.floor(Math.random() * randIngredient.length)];
-const cookingMethod = randCookingMethod[Math.floor(Math.random(Math.floor() * randCookingMethod))];
+const cookingMethod = randCookingMethod[Math.floor(Math.random(Math.floor() * randCookingMethod.length))];
 const utensil = randUtensil[Math.floor(Math.random() * randUtensil.length)];
 
 const randomizedRecipe = `${cookingMethod} the ${ingredient} for ${time} minutes using a ${utensil}.`;
